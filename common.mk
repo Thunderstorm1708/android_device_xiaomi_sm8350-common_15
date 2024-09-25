e7004a972a73bd858310ccb12bc1419b946ef706
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:46 +0300

    sm8350-common: Switch to AIDL USB HALs
    Change-Id: I70f6a4e57eef577d86987bfae44f381a4f107624

--- common.mk
+++ common.mk
@@ -545,7 +545,7 @@ update_engine_client
 
 # USB
 PRODUCT_PACKAGES += \
-android.hardware.usb@1.3-service-qti
+    android.hardware.usb-service.qti
 
 PRODUCT_COPY_FILES += \
 frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
