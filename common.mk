718742b719e0c7a3ad542343112d3146d4611fe6
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:32 +0300

    sm8350-common: Switch to AIDL thermal HAL
    Change-Id: I6ae621194044ab66fad69b9ef997b3b347bf6120

--- common.mk
+++ common.mk
@@ -516,7 +516,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal-service.qti
 
 # TinyXML
 PRODUCT_PACKAGES += \
